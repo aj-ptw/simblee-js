0a95a2562171d691ae3b1bfdf28b89f345ce0bf2
AJ Keller <pushtheworldllc@gmail.com>
2016-08-17 10:15:31 -0400

    Add stream to simulator

--- ganglionSimulator.js
+++ ganglionSimulator.js
@@ -3,20 +3,25 @@ const dgram = require('dgram');
 const udpRx = dgram.createSocket('udp4');
 const udpTx = dgram.createSocket('udp4');
 
+const ganglionSampleRate = 256;
 const udpRxPort = 10996;
 const udpTxPort = 10997;
 
-var udpRxOpen = false;
-
-const UDP_CMD_CONNECT     = "c";
-const UDP_CMD_COMMAND     = "k";
+const GANGLION_CMD_STREAM_START = "b";
+const GANGLION_CMD_STREAM_STOP = "s";
+const UDP_CMD_CONNECT = "c";
+const UDP_CMD_COMMAND = "k";
 const UDP_CMD_DISCONNECT  = "d";
-const UDP_CMD_ERROR       = "e";
-const UDP_CMD_SCAN        = "s";
-const UDP_CMD_STATUS      = "q";
+const UDP_CMD_ERROR = "e";
+const UDP_CMD_SCAN = "s";
+const UDP_CMD_STATUS = "q";
+const UDP_DATA = "t";
 const UDP_STOP = ";\n";
 
-var connected = false;
+let udpRxOpen = false;
+let connected = false;
+let stream;
+let streaming = false;
 
 ///////////////////////////////////////////////////////////////
 // UDP Rx "Server"                                           //
@@ -55,8 +60,7 @@ var parseMessage = function(msg) {
       break;
     case UDP_CMD_COMMAND:
       if (connected) {
-        var buf = new Buffer(`${UDP_CMD_COMMAND},200${UDP_STOP}`);
-        udpTx.send(buf,udpTxPort);
+        parseCommand(String.fromCharCode(msg[1]));
       } else {
         error400();
       }
@@ -71,7 +75,7 @@ var parseMessage = function(msg) {
       }
       break;
     case UDP_CMD_SCAN:
-      var buf = new Buffer(`${UDP_CMD_SCAN},200,ganglion-1234${UDP_STOP}`);
+      var buf = new Buffer(`${UDP_CMD_SCAN},200,ganglion-1234,bose-qc-headphones${UDP_STOP}`);
       udpTx.send(buf,udpTxPort);
       break;
     case UDP_CMD_STATUS:
@@ -90,6 +94,45 @@ var parseMessage = function(msg) {
       break;
   }
 }
+
+var parseCommand = cmd => {
+  switch (cmd) {
+    case GANGLION_CMD_STREAM_START:
+      if (!stream) startStream(); // Starts the stream
+      streaming = true;
+      break;
+    case GANGLION_CMD_STREAM_STOP:
+      if (stream) clearInterval(stream); // Stops the stream
+      streaming = false;
+      break;
+    default:
+      // Send message to tell driver command not recognized
+      udpTx.send(new Buffer(`${UDP_CMD_COMMAND},406${UDP_STOP}`), udpTxPort);
+      break;
+  }
+}
+
+var startStream = () => {
+    const intervalInMS = 1000 / ganglionSampleRate;
+    const bufPre = new Buffer(`${UDP_DATA},`);
+    const bufPost = new Buffer(`,${UDP_STOP}`);
+    let sampleNumber = 0;
+
+    var getSample = sampleNumber => {
+      return new Buffer([sampleNumber, ',', 1, ',', 2, ',', 3, ',', 4]);
+    };
+
+    stream = setInterval(() => {
+        let bufSamp = getSample(sampleNumber);
+        let totalLength = bufPre.length + bufSamp.length + bufPost.length;
+        // Send the packet
+        udpTx.send(Buffer.concat([bufPre,bufSamp,bufPost],totalLength), udpTxPort);
+        // Increment the sample number
+        sampleNumber++;
+    }, intervalInMS);
+};
+
+
 
 function error400() {
   var buf = new Buffer(`${UDP_CMD_ERROR},400,Error: No open BLE device${UDP_STOP}`);
